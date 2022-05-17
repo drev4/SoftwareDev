2b78d887f72533dffebfdd2e2b35192e1d065390
Diego Revuelta <diegorevhoz4@gmail.com>
2022-05-17 22:42:28 +0200

    Add an Acceptance Test example with cucumber

---
+++ DDD-Typescript/tests/apps/blog/backend/features/status.feature
@@ -0,0 +1,8 @@
+Feature: Api Status
+  In order to know the server is up and running
+  As a health check
+  I want to check the api status
+
+  Scenario: Check Api Status
+    Given I send a GET request to "/status"
+    Then the respond status code should be 200
---
+++ DDD-Typescript/tests/apps/blog/backend/features/step_definitions/controller.steps.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import {MoocBackendApp} from '../../../../../../src/apps/blog/backend/MoocBackendApp';
+import {AfterAll, BeforeAll, Given, Then} from 'cucumber';
+
+let _request: request.Test;
+let application: MoocBackendApp;
+
+Given('I send a GET request to {string}', (route: string) => {
+  _request = request(application.httpServer).get(route);
+});
+
+Then('the respond status code should be {int}', async (status: number) => {
+  await _request.expect(status);
+});
+
+BeforeAll(async () => {
+  application = new MoocBackendApp();
+  await application.start();
+});
+
+AfterAll(async () => {
+  await application.stop();
+})
